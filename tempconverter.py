67ecf3f4d333cd18be26d249235888f37f8035af
khaledallen <khaled.allen@gmail.com>
2015-10-26 15:15:04 -0600

    Dad's suggested changes
    Dad's changes focus on more efficiency--less lines to do the same amount of code. He moved some of the logic and consolidated it. Need to study how this works

--- tempconverter.py
+++ tempconverter.py
@@ -1,19 +1,15 @@
 #Get the starting temperature's scale
-input_scale = 'e'
-while input_scale != 'C' and input_scale != 'F':
-    print ('Temperature scale must be F or C.')
-    input_scale = input ('Starting temperature scale (F/C?)')
-    input_scale = input_scale.upper()
-    print (input_scale)
+
+input_scale = raw_input ('Starting temperature scale (F/C?)')
+
+while input_scale.upper() != 'C' and input_scale.upper() != 'F':
+        print (input_scale)
+    print ('Temperature scale must be F or C, Try again')
 
 temp = input ('What is the temperature?')
-if input_scale == 'F':
-        output_scale = 'C'
-        result = (int(temp)-32) * (5/9)
-if input_scale == 'C':
-        output_scale = 'F'
-        result = (int(temp) * 1.8) + 32
+
+result = (((int(temp)-32) * (5/9)) if input_scale == 'F' else ((int(temp) * 1.8) + 32)
 
 #calculate the temperature conversion
-print ('The starting temperature was ' + temp + ' degrees ' + str(input_scale) + '.')
-print ('The temperature in degrees ' + output_scale + ' is ' + str(result) + '.')
+print ('The starting temperature was ' + str(temp) + ' degrees ' + str(input_scale) + '.')
+print ('The temperature in degrees ' + str('F' if input_scale = 'C' else 'F') + ' is ' + str(result) + '.')
