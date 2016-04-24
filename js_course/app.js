38fc54e34cd84069332ee4db1dcb5b9a1dad3dca
khaledallen <khaled.allen@gmail.com>
2016-04-23 19:10:18 -0600

    Created file

---
+++ js_course/app.js
@@ -0,0 +1,8 @@
+//This imports the calc.js file and then calls the various functions inside it using some preset variables.
+
+var calc = require('./calc');
+
+console.log(calc.add(1,2));
+console.log(calc.multiply(1,2));
+console.log(calc.subtract(1,2));
+console.log(calc.divide(1,2));
