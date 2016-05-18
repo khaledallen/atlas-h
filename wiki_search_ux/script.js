be902f82ebf2b2d0f12fb847c98192f221b2e82a
khaledallen <khaled.allen@gmail.com>
2016-05-17 19:36:50 -0700

    Create script.js

---
+++ wiki_search_ux/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+  $("#searchTerm").on("focus", function() {
+    $(this).removeClass("form-start").removeClass("form-close").addClass("form-open").attr("placeholder","");
+    $("#send").removeClass("btn-start fadeOut").addClass("animated fadeIn");
+  });
+  $("#searchTerm").on("focusout", function() {
+    $(this).removeClass("form-open").addClass("form-close").addClass("form-start");
+    $("#send").removeClass("fadeIn").addClass("fadeOut");
+  });
+
+  $("#form").submit(function(e) {
+    e.preventDefault();
+    $(".results").html("");
+    var apiCall = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=" + $("#searchTerm").val() + "&callback=?";
+
+    $.getJSON(apiCall, function(data) {
+      console.log(data);
+      $.each(data.query.search, function(p, i) {
+        console.log(i);
+        var foundItem = '<div class="foundItem foundItem-' + i + ' animated fadeIn"><h2><a href="https://en.wikipedia.org/wiki/' + i.title.replace(" ", "_") + '" target="blank">' + i.title + '</a></h2><p>' + i.snippet + '</div>';
+        $(foundItem).appendTo(".results");
+      });
+    }).fail(function(jqxhr, textStatus, error) {
+      var err = textStatus + ", " + error;
+      $(err).appendTo(".results");
+    });
+  });
+
+  $("#randomGen").on("click", function() {
+    $(".results").html("");
+    var apiCall = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&callback=?&utf8=1&formatversion=2&rnnamespace=0&rnlimit=10";
+
+    $.getJSON(apiCall, function(data) {
+      console.log(data);
+      $.each(data.query.random, function(p, i) {
+        console.log(i);
+        var foundItem = '<div class="foundItem foundItem-' + i + ' animated fadeIn"><h2><a href="https://en.wikipedia.org/wiki/' + i.title.replace(" ", "_") + '" target="blank">' + i.title + '</a></h2><p></div>';
+        $(foundItem).appendTo(".results");
+      });
+    }).fail(function(jqxhr, textStatus, error) {
+      var err = textStatus + ", " + error;
+      $(err).appendTo(".results");
+    });
+  });
+
+});
+
+// This is the API call for a random article.
